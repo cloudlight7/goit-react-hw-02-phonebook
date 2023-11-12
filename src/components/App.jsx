21fd9227ce10dead861888322a7471855652efff
cloudlight7 <cloudlight7@gmail.com>
2023-11-13 00:34:19 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,13 +12,7 @@ import {Sections} from './SectionStyle'
 //import { Wrapper } from './StyleComponent'
 export class App extends Component {
 state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    {id: 'id-5', name: 'Anniee Copelaned', number: '2227-91-26'},
-  ],
+  contacts: [],
   filter: '',
 }
   addContact = (newContact) => {
