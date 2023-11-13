0116bfb820317126571fe8eedf96053445ccdd40
cloudlight7 <cloudlight7@gmail.com>
2023-11-13 08:25:36 +0200

    fix filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,14 @@ import ContactList from './ContactList'
 import { nanoid } from 'nanoid'
 import Filter from './Filter'
 import {Sections} from './SectionStyle'
-//import FeedbackOptions from './FeedbackOptions'
-//import Section from './Section'
-//import Statistics from './Statistics'
-//import Notification from './Notification'
-//import { Wrapper } from './StyleComponent'
 export class App extends Component {
 state = {
-  contacts: [],
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
   filter: '',
 }
   addContact = (newContact) => {
@@ -36,19 +36,20 @@ state = {
   }
   
   filter = ({ target: { value } }) => {
-      this.setState({filter:this.state.contacts.filter(name => name.name.toLowerCase().includes(value.toLowerCase()))})
-
-}
+     this.setState({ filter: value});
+  }
+  contactsList = () => {
+    return  this.state.contacts.filter(name => name.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+  }
   render() {
     return (
-      
 <Sections>
   <h1>Phonebook</h1>
  <ContactForm addContact={this.addContact} />
 
         <h2>Contacts</h2>
         <Filter filter={this.filter} />
-        <ContactList contacts={this.state.filter? this.state.filter : this.state.contacts } delContact={this.delContact} />
+        <ContactList contacts={this.contactsList()} delContact={this.delContact} />
 </Sections>
   );
   };
